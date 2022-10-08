15cc5621b445c01ed9a576592dcd76bb74986c2a
Kerem Erguner <ergunerkerem@hotmail.com>
2022-10-08 19:50:20 +0300

    firebase auth module initial

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-const Banane = () => (
+const Banane = () => {
 
-  <SafeAreaView>
-    <Text>Banane</Text>
-  </SafeAreaView>
-);
+
+  const signUp = () => {
+    auth()
+      .createUserWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!'
+      )
+      .then(res => console.log('response:' + res))
+      .catch(err => console.log('error: ' + err))
+  }
+
+  const signIn = () => {
+    auth()
+      .signInWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!'
+      )
+      .then(console.log('Sign In'))
+      // .then(res => console.log('response:' + JSON.stringify(res)))
+      .catch(err => console.log('error: ' + err))
+  }
+
+  const signOut = () => {
+    auth()
+      .signOut()
+      .then(console.log('Sign Out'))
+      // .then(res => console.log('Sign Out:' + JSON.stringify(res)))
+      .catch(err => console.log('error: ' + err))
+  }
+
+  const checkUser = () => {
+    const user = auth().currentUser;
+    console.log(user)
+  }
+
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 60, color: 'red' }}>BANANE</Text>
+      <Button title='Sign Up' onPress={signUp} />
+      <Button title='Sign In' onPress={signIn} />
+      <Button title='Sign Out' onPress={signOut} />
+      <Button title='Check User' onPress={checkUser} />
+
+    </SafeAreaView>
+  )
+}
 
 export default Banane;
