42d8ad20d434fb3fe8757d5c9cb37358f986043c
Kerem Erguner <ergunerkerem@hotmail.com>
2022-10-09 15:47:26 +0300

    DB example initial

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaView, Text, View, Button } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 
 const Banane = () => {
 
@@ -39,6 +40,51 @@ const Banane = () => {
     console.log(user)
   }
 
+  const checkDb = () => {
+    const reference = database().ref('books');
+    reference
+      .once('value')
+      .then(snapshot => {
+        const response = snapshot.val();
+        console.log(response)
+      })
+  }
+  const listenDb = () => {
+    const reference = database().ref('books/1');
+    reference
+      .on('value', snapshot => {
+        console.log(snapshot.val())
+      })
+  }
+
+  const setDb = () => {
+    const reference = database().ref('car/rentable');
+    reference
+      .set({
+        brand: 'Audi',
+        model: 'A6',
+        price: '128.000'
+      })
+  }
+
+  const updateDb = () => {
+    const reference = database().ref('car/rentable');
+    reference
+      .update({
+        model: 'A8'
+      })
+  }
+
+  const pushDb = () => {
+    const reference = database().ref('car');
+    reference
+      .push({
+        brand: 'Bmw',
+        model: '320',
+        price: '670.000'
+      })
+  }
+
   return (
     <SafeAreaView>
       <Text style={{ fontSize: 60, color: 'red' }}>BANANE</Text>
@@ -46,6 +92,11 @@ const Banane = () => {
       <Button title='Sign In' onPress={signIn} />
       <Button title='Sign Out' onPress={signOut} />
       <Button title='Check User' onPress={checkUser} />
+      <Button title='Check DB' onPress={checkDb} />
+      <Button title='Listen DB' onPress={listenDb} />
+      <Button title='Set DB' onPress={setDb} />
+      <Button title='Update DB' onPress={updateDb} />
+      <Button title='Push DB' onPress={pushDb} />
 
     </SafeAreaView>
   )
