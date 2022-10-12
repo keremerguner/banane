e5f7dbb14791334b265a4c9ea097e34efe9c64fe
Kerem Erguner <ergunerkerem@hotmail.com>
2022-10-12 23:55:20 +0300

    vector icons added - react native de IOS BUILD FAILED veriyo onun yerine gidip xcode'dan calistiriyoruz

--- src/pages/auth/Login/Login.js
+++ src/pages/auth/Login/Login.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 import Input from '../../../components/Input'
 import Button from '../../../components/Button'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Login = (props) => (
     <SafeAreaView>
         {/* <Button title='Press' onPress={() => props.navigation.navigate('SignPage')} */}
+            <Text>
+                <Icon name="rocket" size={30} color="#900" />
+            </Text>
         <Input placeholder='e-posta giriniz...' />
         <Input placeholder='sifre giriniz...' />
         <Button text='Giriş Yap' />
